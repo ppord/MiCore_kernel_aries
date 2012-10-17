43290552006da057f16d545956c9dec679c45aee
Hemant Kumar <hemantk@codeaurora.org>
2012-10-17 12:29:51 -0700

    EHCI: HSIC: Allow standalone power collapse when HSIC is active
    HSIC generates interrupt at 4ms interval. If idle power collapse
    is enabled when HSIC is active, krait keeps on going into idle
    power collapse requesting RPM to handle the sleep set requirement.
    If HSIC interrupt comes after 4ms interval, krait comes out of idle
    power collapse and it again ask RPM to handle this request therefore
    keeps RPM busy handling requests due to frequent idle power collapse.
    This has a power impact. Hence only allow standalone power collapse
    when HSIC is active to avoid RPM involvement in assisting kraits to
    power collapse. Vote for idle power collapse when HSIC goes to low
    power mode.
    
    Change-Id: Iee8bc1fa180499f2d26fbac955a6a6ed767fdeb4
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    	arch/arm/mach-msm/lge/mako/board-mako.c
    
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    	arch/arm/mach-msm/lge/mako/board-mako.c

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -738,11 +738,16 @@ static int msm_hsic_resume(struct msm_hsic_hcd *mehci)
 	unsigned temp;
 	int min_vol, max_vol;
 	unsigned long flags;
+	struct msm_hsic_host_platform_data *pdata = mehci->dev->platform_data;
 
 	if (!atomic_read(&mehci->in_lpm)) {
 		dev_dbg(mehci->dev, "%s called in !in_lpm\n", __func__);
 		return 0;
 	}
+
+	if (pdata && pdata->standalone_latency)
+		pm_qos_update_request(&mehci->pm_qos_req_dma,
+			pdata->standalone_latency + 1);
 
 	spin_lock_irqsave(&mehci->wakeup_lock, flags);
 	if (mehci->wakeup_irq_enabled) {
@@ -1036,9 +1041,9 @@ resume_again:
 				pm_qos_update_request(&mehci->pm_qos_req_dma,
 					pdata->swfi_latency + 1);
 			wait_for_completion(&mehci->gpt0_completion);
-			if (pdata && pdata->swfi_latency)
+			if (pdata && pdata->standalone_latency)
 				pm_qos_update_request(&mehci->pm_qos_req_dma,
-					PM_QOS_DEFAULT_VALUE);
+					pdata->standalone_latency + 1);
 			spin_lock_irq(&ehci->lock);
 		} else {
 			dbg_log_event(NULL, "FPR: Tightloop", 0);
@@ -1685,9 +1690,9 @@ static int __devinit ehci_hsic_msm_probe(struct platform_device *pdev)
 
 	__mehci = mehci;
 
-	if (pdata && pdata->swfi_latency)
+	if (pdata && pdata->standalone_latency)
 		pm_qos_add_request(&mehci->pm_qos_req_dma,
-			PM_QOS_CPU_DMA_LATENCY, PM_QOS_DEFAULT_VALUE);
+			PM_QOS_CPU_DMA_LATENCY, pdata->standalone_latency + 1);
 
 	/*
 	 * This pdev->dev is assigned parent of root-hub by USB core,
@@ -1726,7 +1731,7 @@ static int __devexit ehci_hsic_msm_remove(struct platform_device *pdev)
 	struct msm_hsic_hcd *mehci = hcd_to_hsic(hcd);
 	struct msm_hsic_host_platform_data *pdata = mehci->dev->platform_data;
 
-	if (pdata && pdata->swfi_latency)
+	if (pdata && pdata->standalone_latency)
 		pm_qos_remove_request(&mehci->pm_qos_req_dma);
 
 	if (mehci->peripheral_status_irq)
